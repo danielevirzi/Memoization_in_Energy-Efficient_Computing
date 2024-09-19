c3fd40b05996bcbefcafb7d260c0516a83a2b84f
Daniele <163782529+danielevirzi@users.noreply.github.com>
2024-09-19 23:55:56 +0200

    add fibonacci.py

---
+++ 99_problems_but_greenlab_aint_one/ricorsiva/fibonacci.py
@@ -0,0 +1,34 @@
+from functools import cache, lru_cache
+
+
+#### Fibonacci sequence ####
+
+def fibonacci(n: int) -> int:
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+@cache
+def fibonacci_cache(n: int) -> int:
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci_cache(n - 1) + fibonacci_cache(n - 2)
+
+
+@lru_cache
+def fibonacci_lru_cache(n: int) -> int:
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci_lru_cache(n - 1) + fibonacci_lru_cache(n - 2)
+
+
+
+
+
