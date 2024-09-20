d6749d2b09cc54e3f6efa0edd2a36e65b823a2ee
Quartinocci <96389802+Quartinocci@users.noreply.github.com>
2024-09-20 13:09:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ from 99_problems_but_greenlab_aint_one import cpu
 from 99_problems_but_greenlab_aint_one import memory
 from 99_problems_but_greenlab_aint_one import ricorsiva
 
+"""
+from pyJoules.energy_meter import measure_energy
+@measure_energy
+def foo():
+	# Instructions to be evaluated.
+
+foo()
+"""
 
 print(cpu.is_prime(29)) 
