321580adcd9e2319bb44c3e4111293754ea971f1
ivyruiwang <ivy.ruiwang@gmail.com>
2024-10-18 22:10:01 +0200

    commit

--- profiling-using-exp-runner/packages/recursive/hanoi.py
+++ profiling-using-exp-runner/packages/recursive/hanoi.py
@@ -25,35 +25,35 @@ def measure_time(func: callable, *args, **kwargs):
 # Basic Implementation
 def tower_of_hanoi(n: int, source: str, target: str, auxiliary: str) -> None:
     if n == 1:
-        print(f"Move disk 1 from {source} to {target}")
+        # print(f"Move disk 1 from {source} to {target}")
         return 
     tower_of_hanoi(n-1, source, auxiliary, target)
-    print(f"Move disk {n} from {source} to {target}")
+    # print(f"Move disk {n} from {source} to {target}")
     tower_of_hanoi(n-1, auxiliary, target, source)
     
 # Using functools.cache (Python 3.9+)
 @cache
 def tower_of_hanoi_cache(n: int, source: str, target: str, auxiliary: str) -> None:
     if n == 1:
-        print(f"Move disk 1 from {source} to {target}")
+        # print(f"Move disk 1 from {source} to {target}")
         return
     tower_of_hanoi_cache(n-1, source, auxiliary, target)
-    print(f"Move disk {n} from {source} to {target}")
+    # print(f"Move disk {n} from {source} to {target}")
     tower_of_hanoi_cache(n-1, auxiliary, target, source)
     
 # Using functools.lru_cache
 @lru_cache(maxsize=None)
 def tower_of_hanoi_lru_cache(n: int, source: str, target: str, auxiliary: str) -> None:
     if n == 1:
-        print(f"Move disk 1 from {source} to {target}")
+        # print(f"Move disk 1 from {source} to {target}")
         return
     tower_of_hanoi_lru_cache(n-1, source, auxiliary, target)
-    print(f"Move disk {n} from {source} to {target}")
+    # print(f"Move disk {n} from {source} to {target}")
     tower_of_hanoi_lru_cache(n-1, auxiliary, target, source)
     
 if __name__ == '__main__':
     # Example usage
-    n = 3
+    n = 25
     source = "A"
     target = "C"
     auxiliary = "B"
