2673b1aeb7154fa0669162279b37a405ce51b14f
ivyruiwang <ivy.ruiwang@gmail.com>
2024-10-24 05:40:39 +0200

    modify time interval

--- profiling-using-exp-runner/profile-recursive-fibonacci/RunnerConfig.py
+++ profiling-using-exp-runner/profile-recursive-fibonacci/RunnerConfig.py
@@ -29,7 +29,7 @@ class RunnerConfig:
     sampling_rate_options = [200]
     """The time Experiment Runner will wait after a run completes.
     This can be essential to accommodate for cooldown periods on some systems."""
-    time_between_runs_in_ms:    int             = 1000 # should be 60000, 1minute
+    time_between_runs_in_ms:    int             = 60000 # should be 60000, 1minute
 
     """The path in which Experiment Runner will create a folder with the name `self.name`, in order to store the
     results from this experiment. (Path does not need to exist - it will be created if necessary.)
